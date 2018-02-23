dc3f0d599639275884d53933d47ff207304f64a9
ibrahim-1996 <ibrahim.1996@outlook.com>
2018-02-23 08:15:16 +0200

    Add more functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-// Selectors
+// Selectors & Global vars
 const deck = document.querySelector('.deck');
 const cards = document.querySelectorAll('.card');
-var compareRoom = [];
-
+const movesSpan = document.querySelector('.moves');
+const restartBtn = document.querySelector('.restart');
+var moves, matched, accuracy, compareRoom;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -19,7 +20,7 @@ function shuffle(array) {
     return array;
 }
 
-// Transition animation
+// Transitions
 function open(card) {
     transition.begin(card, ["transform", "rotateY(0)", "rotateY(180deg)", "500ms", "linear"]);
     setTimeout(() => card.classList.add("open"), 250);
@@ -34,34 +35,65 @@ function match() {
     [...compareRoom].forEach(card => card.classList.add("match"))
 }
 
+function noMatch() {
+    [...compareRoom].forEach(card => close(card));
+}
+
 // Comparison logic
 function compare() {
     if (compareRoom.length === 2) {
         if (compareRoom[0].firstElementChild.classList.value === compareRoom[1].firstElementChild.classList.value) {
             match();
+            matched++;
         } else {
-            [...compareRoom].forEach(card => close(card));
+            noMatch();
+            accuracy--;
         }
         compareRoom = [];
-    } else {
-        null
     }
+    // Todo: if complete show a modal
 }
 
-// Update HTML
-deck.innerHTML = '';
-shuffle([...cards]).forEach(card => deck.appendChild(card));
+function updateScore() {
+    movesSpan.textContent = moves;
+    // Todo: update stars
+}
 
-// Open cards on new game
-cards.forEach(card => open(card));
+function newGame() {
 
-// Close cards after 2 secs
-setTimeout(() => cards.forEach(card => close(card)), 2000);
+    // Intialize all counters
+    moves = 0, matched = 0, accuracy = 30, compareRoom = [];
 
-//Set up the event listener for a card. If a card is clicked:
+    // Reset HTML
+    deck.innerHTML = '';
+    shuffle([...cards]).forEach(card => {
+        card.classList.value = ('card');
+        deck.appendChild(card)
+    });
+    movesSpan.textContent = moves
+
+    // Open cards
+    cards.forEach(card => open(card));
+
+    // Close cards after 2 secs
+    setTimeout(() => cards.forEach(card => close(card)), 2000);
+}
+
+// Card click event
 deck.addEventListener('click', function (event) {
-    event.target.classList.contains('card') ? (open(event.target), compareRoom.push(event.target)) : null;
-    setTimeout(() => compare(), 500);
+    if (event.target.classList.value === 'card') {
+        open(event.target);
+        compareRoom.push(event.target);
+        moves++;
+        updateScore();
+        setTimeout(() => compare(), 500);
+    }
 });
 
-// if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+// Restart click event
+restartBtn.addEventListener('click', function (event) {
+    newGame();
+});
+
+// Let the game begin
+newGame();
